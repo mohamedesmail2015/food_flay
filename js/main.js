1fb6808b6efe1ead3302717e8f6691ea4c1b50b1
Mohamed <mohamedmahmoudhassan2015@gmail.com>
2022-12-19 01:40:53 +0200

    your commit

--- js/main.js
+++ js/main.js
@@ -207,11 +207,11 @@ function initMap() {
 
 // *********************************
 
-filterSelection('all')
+ filterSelection('all')
 function filterSelection(c) {
   var x, i
-  x = document.getElementsByClassName('column')
-  if (c == 'all') c = ''
+  x = document.getElementsByClassName('column');
+  //if (c == 'all') c = ''
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], 'show')
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], 'show')
